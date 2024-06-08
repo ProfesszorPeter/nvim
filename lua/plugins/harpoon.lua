74c6fc07652c6b3ee92218025ae05a135a934a49
ProfesszorPeter <simonpetymeg@gmail.com>
2024-06-08 20:47:35 +0200

    harpoon and telescope fixed

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,24 +1,22 @@
-return{
-	'ThePrimeagen/harpoon',
+return {
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  config = function()
+    local harpoon = require "harpoon"
+    harpoon:setup()
 
-	dependencies = {
-		"nvim-lua/plenary.nvim"
-	},
-	config = function()
-		local harpoon = require("harpoon")
+    vim.keymap.set("n", "<leader>a", function()
+      harpoon:list():add()
+    end)
+    vim.keymap.set("n", "<C-e>", function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end)
 
-		harpoon:setup()
-
-		vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-		vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-
-		vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-		vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-		vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-		vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
-		vim.keymap.set("n", "<leader><C-h>", function() harpoon:list():replace_at(1) end)
-		vim.keymap.set("n", "<leader><C-t>", function() harpoon:list():replace_at(2) end)
-		vim.keymap.set("n", "<leader><C-n>", function() harpoon:list():replace_at(3) end)
-		vim.keymap.set("n", "<leader><C-s>", function() harpoon:list():replace_at(4) end)
-	end
+    -- Set <space>1..<space>5 be my shortcuts to moving to the files
+    for _, idx in ipairs { 1, 2, 3, 4, 5 } do
+      vim.keymap.set("n", string.format("<space>%d", idx), function()
+        harpoon:list():select(idx)
+      end)
+    end
+  end,
 }
--- lua/spater/remap.lua
+++ lua/spater/remap.lua
@@ -48,12 +48,21 @@ vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
 
+--    local harpoon = require "harpoon"
+--    harpoon:setup()
+--vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+--vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+--vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+--vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+--vim.keymap.set("n", "<leader><C-h>", function() harpoon:list():replace_at(1) end)
+--vim.keymap.set("n", "<leader><C-t>", function() harpoon:list():replace_at(2) end)
+--vim.keymap.set("n", "<leader><C-n>", function() harpoon:list():replace_at(3) end)
+--vim.keymap.set("n", "<leader><C-s>", function() harpoon:list():replace_at(4) end)
 
 
-
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+--local builtin = require('telescope.builtin')
+--vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+--vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+--vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+--vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
