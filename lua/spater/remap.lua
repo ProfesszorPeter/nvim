cded4b8acd04bcbf0c352ad8e2e9b07abaa71a24
ProfesszorPeter <simonpetymeg@gmail.com>
2024-06-09 12:59:03 +0200

    keybindings

--- lua/spater/remap.lua
+++ lua/spater/remap.lua
@@ -48,21 +48,21 @@ vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
 
---    local harpoon = require "harpoon"
---    harpoon:setup()
---vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
---vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
---vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
---vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
---vim.keymap.set("n", "<leader><C-h>", function() harpoon:list():replace_at(1) end)
---vim.keymap.set("n", "<leader><C-t>", function() harpoon:list():replace_at(2) end)
---vim.keymap.set("n", "<leader><C-n>", function() harpoon:list():replace_at(3) end)
---vim.keymap.set("n", "<leader><C-s>", function() harpoon:list():replace_at(4) end)
+--telescope--
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>pws', function()
+    local word = vim.fn.expand("<cword>")
+    builtin.grep_string({ search = word })
+end)
+vim.keymap.set('n', '<leader>pWs', function()
+    local word = vim.fn.expand("<cWORD>")
+    builtin.grep_string({ search = word })
+end)
+vim.keymap.set('n', '<leader>ps', function()
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end)
+vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
 
-
---local builtin = require('telescope.builtin')
---vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
---vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
---vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
---vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-
+--harpoon--
