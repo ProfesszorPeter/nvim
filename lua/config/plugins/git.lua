bff29893046d7f681aff406323e832ef718c667a
ProfesszorPeter <simonpetymeg@gmail.com>
2025-01-19 23:00:24 +0100

    keymaps

--- lua/config/plugins/git.lua
+++ lua/config/plugins/git.lua
@@ -1,6 +1,7 @@
 return{
   "tpope/vim-fugitive",
   vim.keymap.set("n","gs", ":Git<CR>"),
-  vim.keymap.set("n","gc", ":Git commit<CR>");
-  vim.keymap.set("n","gp", ":Git push<CR>")
+  vim.keymap.set("n","gc", ":Git commit<CR>"),
+  vim.keymap.set("n","gp", ":Git push<CR>"),
+  vim.keymap.set("n","gb", ":Git blame<CR>")
 }
