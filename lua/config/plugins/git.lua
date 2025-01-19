21deea61746c1f0146150ffd5596cebd7870a366
ProfesszorPeter <simonpetymeg@gmail.com>
2025-01-19 23:20:26 +0100

    addig add

--- lua/config/plugins/git.lua
+++ lua/config/plugins/git.lua
@@ -3,5 +3,6 @@ return{
   vim.keymap.set("n","gs", ":Git<CR>"),
   vim.keymap.set("n","gc", ":Git commit<CR>"),
   vim.keymap.set("n","gp", ":Git push<CR>"),
-  vim.keymap.set("n","gb", ":Git blame<CR>")
+  vim.keymap.set("n","gb", ":Git blame<CR>"),
+  vim.keymap.set("n","ga", ":Git add .<CR>")
 }
