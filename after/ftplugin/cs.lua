8727d7c1677cccfd82b908088888167cc780bb59
ProfesszorPeter <simonpetymeg@gmail.com>
2025-01-19 22:37:31 +0100

    started to coloring

--- after/ftplugin/cs.lua
+++ after/ftplugin/cs.lua
@@ -1,9 +1,16 @@
+local set = vim.opt_local
+
+vim.opt.cursorline = true
+set.shiftwidth = 4
+set.number = true
+set.relativenumber = true
+
+config = function()
+  local capabilities = require('blink.cmp').get_lsp_capabilities()
+  require('lspconfig').csharp_ls.setup { capabilites = capabilities }
+  require 'lspconfig'.omnisharp.setup { capabilites = capabilities }
+end
 vim.cmd [[hi @function.builtin.lua guifg=turquoise]]
 vim.cmd [[hi @keyword.conditional.lua guifg=blueviolet]]
 vim.cmd [[hi @boolean.lua guifg=blue]]
-
-local set = vim.opt_local
-
-set.shiftwidth = 2
-set.number = true
-set.relativenumber = true
+vim.cmd [[hi DiagnosticUnderlineError guifg=red]]
