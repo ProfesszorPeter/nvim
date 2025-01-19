eea1ee13a2ca6b33c93019c4ee2edda7ff05a993
ProfesszorPeter <simonpetymeg@gmail.com>
2025-01-19 22:46:49 +0100

    mason & keymaps

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -33,21 +33,41 @@ return {
       }
       require 'lspconfig'.dockerls.setup {}
 
-
-      vim.api.nvim_create_autocmd('LspAttach', {
-        callback = function(args)
-          local c = vim.lsp.get_client_by_id(args.data.client_id)
-          if not c then return end
-
-          -- Format the current buffer on save
-          vim.api.nvim_create_autocmd('BufWritePre', {
-            buffer = args.buf,
-            callback = function()
-              vim.lsp.buf.format({ bufnr = args.buf, id = c.id })
-            end,
-          })
-        end,
+      require("mason").setup({
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+          }
+        }
       })
+      --vim.api.nvim_create_autocmd('LspAttach', {
+      --callback = function(args)
+      --local c = vim.lsp.get_client_by_id(args.data.client_id)
+      --if not c then return end
+
+      ---- Format the current buffer on save
+      --vim.api.nvim_create_autocmd('BufWritePre', {
+      --buffer = args.buf,
+      --callback = function()
+      --vim.lsp.buf.format({ bufnr = args.buf, id = c.id })
+      --end,
+      --})
+      --end,
+      --})
+      local builtin = require "telescope.builtin"
+
+          vim.keymap.set("n", "gd", builtin.lsp_definitions)
+          vim.keymap.set("n", "gr", builtin.lsp_references, { buffer = 0 })
+          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = 0 })
+          vim.keymap.set("n", "gD", vim.lsp.buf.definition)
+          vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, { buffer = 0 })
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
+
+          vim.keymap.set("n", "<space>cr", vim.lsp.buf.rename, { buffer = 0 })
+          vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, { buffer = 0 })
+          vim.keymap.set("n", "<space>wd", builtin.lsp_document_symbols, { buffer = 0 })
     end,
   }
 }
