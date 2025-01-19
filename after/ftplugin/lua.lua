41705b29207cfa16e62eefe6043382299f83d70b
ProfesszorPeter <simonpetymeg@gmail.com>
2025-01-19 22:40:27 +0100

    lsp & color

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -2,11 +2,14 @@ local set = vim.opt_local
 
 set.shiftwidth = 2
 
-if true then
-  return
+config = function()
+  local capabilities = require('blink.cmp').get_lsp_capabilities()
+  require 'lspconfig'.lua_ls.setup { capabilites = capabilities }
 end
-vim.cmd [[hi @function.builtin.lua guifg=turquoise]]
+
+vim.cmd [[hi @lsp.type.function.lua guifg=turquoise]]
+vim.cmd [[hi @lsp.type.variable.lua guifg=#93CCEA]]
 vim.cmd [[hi @keyword.conditional.lua guifg=blueviolet]]
 vim.cmd [[hi @boolean.lua guifg=blue]]
 
-vim.cmd [[hi @variable.lua guifg=black]]
+vim.cmd [[hi DiagnosticUnderlineError guifg=brown]]
